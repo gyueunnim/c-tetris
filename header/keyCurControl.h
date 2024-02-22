93c472328bc745b99ba1ab2b38c514e1cb8d762a
gyueunnim <rnrmfrbdms@gmail.com>
2024-02-22 14:00:57 +0900

    fix: 커서, 키보드 컨트롤 함수 헤더 추가

---
+++ header/keyCurControl.h
@@ -0,0 +1,12 @@
+// 커서, 키보드 컨트롤 함수들 정의
+
+#ifndef __KEY_CUR_CONTROL_H_
+#define __KEY_CUR_CONTROL_H_
+
+#include "point.h"
+
+void RemoveCursor(void);
+point GetCurrentCursorPos(void);
+void SetCurrentCursorPos(int x, int y);
+
+#endif
