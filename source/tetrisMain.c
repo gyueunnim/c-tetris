23ec5f1a0f0f06d3cf10f467942482c9f39e1e25
gyueunnim <rnrmfrbdms@gmail.com>
2024-02-24 19:36:57 +0900

    feat: tetrisMain 수정

--- source/tetrisMain.c
+++ source/tetrisMain.c
@@ -1,23 +1,41 @@
 #include <Windows.h>
+#include "common.h"
 #include "keyCurControl.h"
 #include "blockStageControl.h"
+#include "scoreLevelControl.h"
 
 #define START_CURPOS_X (5 * 2)
 #define START_CURPOS_Y (0)
-#define SYS_DELAY	   100
 
 int main(void) {
 	RemoveCursor();
 
-	InitNewBlockPos(START_CURPOS_X, START_CURPOS_Y);
+	InitKeyDelayRate(10); // 게임 속도 설정
 
-	ChooseBlock();
+	DrawGameBoard();
+
+	ShowCurrentScoreAndLevel() ;
 
 	while (1) {
-		BlockDown();
+		InitNewBlockPos(START_CURPOS_X, START_CURPOS_Y);
+		   
+		ChooseBlock();
 
-		Sleep(SYS_DELAY);
+		if (IsGameOver())
+			break; 
+
+		while (1) { 
+			if (BlockDown() == 0) {
+				// AddCurrentBlockInfoToBoard();
+				break;
+			}
+			if (ProcessKeyInput())
+				break;
+		}
 	}
+	 
+	SetCurrentCursorPos(10, 10);
+	puts("GAME OVER ^^");
 
 	return 0;
 }
